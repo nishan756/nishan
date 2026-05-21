1fe4cf37eabf04a2f10cd89e51399a37a85c56c2
@nishan <aanishan339@gmail.com>
2026-05-21 20:52:18 +0600

    Update API_URL to new backend endpoint

--- articles.js
+++ articles.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/app/articles/";
+const API_URL = "nishan-backend-hc51.onrender.com";
 const container = document.getElementById("articlesContainer");
 const loading = document.getElementById("loading");
 
